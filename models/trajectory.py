ca4f121913d330c1f0d51a94dd9f1daf5d8ad8af
makuksik <mateusz.petelicki@gmail.com>
2025-10-04 14:08:16 +0200

    FIX: ZMIANA PLANÓW XDDDDDDDD

--- models/trajectory.py
+++
@@ -1,7 +0,0 @@
-class Trajectory:
-    def __init__(self, body, path=None):
-        self.body = body  # Asteroid or Planet
-        self.path = path if path else []
-
-    def record_position(self):
-        self.path.append(tuple(self.body.position))
