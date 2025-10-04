6e411b52b57e8717907ca1beae83c9b4b7247603
makuksik <mateusz.petelicki@gmail.com>
2025-10-04 12:14:57 +0200

    Changes asteroid and planet classes

--- models/planet.py
+++ models/planet.py
@@ -1,6 +1,13 @@
+from vpython import sphere, vector, color
+
+
 class Planet:
-    def __init__(self, name, mass, radius, position):
+    def __init__(self, name, radius, mass, position=(0, 0, 0), color_=color.blue):
         self.name = name
-        self.mass = mass
         self.radius = radius
-        self.position = position 
+        self.mass = mass
+        self.position = vector(*position)
+        self.body = sphere(pos=self.position, radius=self.radius, color=color_, opacity=0.6)
+
+    def __str__(self):
+        return f"{self.name}: radius={self.radius}, mass={self.mass}, position=({self.position.x},{self.position.y},{self.position.z})"
